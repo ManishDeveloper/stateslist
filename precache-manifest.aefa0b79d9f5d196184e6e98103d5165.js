f06acc5ee1f86e85747f219eb35b9a344ab9b698
Manish Bharti <manishbharti836@gmail.com>
2020-05-01 06:47:23 +0530

    Updates

---
+++ precache-manifest.aefa0b79d9f5d196184e6e98103d5165.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1904828e4ab2df478095039e13afc2",
+    "url": "/stateslist/index.html"
+  },
+  {
+    "revision": "0101509c7f57f63c7681",
+    "url": "/stateslist/static/css/main.201e6e6d.chunk.css"
+  },
+  {
+    "revision": "0d85634b9b25eef14f91",
+    "url": "/stateslist/static/js/2.2c7ef734.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/stateslist/static/js/2.2c7ef734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0101509c7f57f63c7681",
+    "url": "/stateslist/static/js/main.5abb3862.chunk.js"
+  },
+  {
+    "revision": "bed6e7a6173f1be7c66b",
+    "url": "/stateslist/static/js/runtime-main.893b898a.js"
+  },
+  {
+    "revision": "36fc1e4bf57d2be14a56518c5e060453",
+    "url": "/stateslist/static/media/background.36fc1e4b.jpg"
+  }
+]);
